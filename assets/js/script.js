6c70ad34613a1a21ee871b306058739252f82682
lydiawdesign <lydia.williamson.OSU@gmail.com>
2021-09-06 20:43:42 -0400

    added global variables to js file

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var citySearch = $("#searchCity");
+var cityInput = $("#cityInput");
+var currentWeather = $("#currentWeather");
+var cityResult = $("#cityResult");
+var forecast = $("#forecast");
+var fiveDayForecast = $("#fiveDayForecast");
+var pastSearches = $("#pastSearches");
+
+var cities= [];
